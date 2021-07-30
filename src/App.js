110b542dc0b2633ea541e29d0ee973bf2978321a
Tabean19 <Tabean19@gmail.com>
2021-07-30 00:19:06 -0500

    actions, reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, Fragment } from 'react';
 import SearchBar from '../src/components/layout/SearchBar';
-import Logs from '../src/components/layout/logs/Logs';
+import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
-import AddLogModal from './components/layout/logs/AddLogModal';
-import EditLogModal from './components/layout/logs/EditLogModal';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
 import { Provider } from 'react-redux';
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,15 @@
+import { bindActionCreators } from 'redux';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+} from '../actions/types';
+
 const initialState = {
   logs: null,
   current: null,
@@ -7,6 +19,52 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
+        loading: false,
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((log) => log.id !== action.payload),
+        loading: false,
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map((log) =>
+          log.id === action.payload.id ? action.payload : log
+        ),
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case LOGS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return state;
   }
